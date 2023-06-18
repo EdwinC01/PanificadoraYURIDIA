6130dc66573bd1ae344ac03a927668330d33e0de
EdwinC01 <116897907+EdwinC01@users.noreply.github.com>
2023-06-17 22:10:19 -0600

    Update MainActivity.java
    Se agregó un comentario en MainActivity

--- app/src/main/java/com/example/panificadorayuridia/MainActivity.java
+++ app/src/main/java/com/example/panificadorayuridia/MainActivity.java
@@ -30,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     * título de la app*/
     String Title = "Inicio de Sesión";
 
+    //Comentario GitHub
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
